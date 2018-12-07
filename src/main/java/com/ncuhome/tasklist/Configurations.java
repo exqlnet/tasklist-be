668913eb67727b83b3997196ea34ffaa3050d2a7
李聪 <591210216@qq.com>
2018-12-07 22:50:19 +0800

    add cors

--- src/main/java/com/ncuhome/tasklist/Configurations.java
+++ src/main/java/com/ncuhome/tasklist/Configurations.java
@@ -4,13 +4,26 @@ package com.ncuhome.tasklist;
 import com.ncuhome.tasklist.util.AuthInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class Configurations implements WebMvcConfigurer {
 
-
+    @Bean
+    public CorsFilter corsFilter() {
+        final UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+        final CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+        urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+        return new CorsFilter(urlBasedCorsConfigurationSource);
+    }
     @Bean
     AuthInterceptor getAuthInterceptor(){
         return new AuthInterceptor();
