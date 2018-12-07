3eac7cbbe9b32e68939b2ae39ed35dfe40a46d71
李聪 <591210216@qq.com>
2018-12-07 23:51:10 +0800

    less 500

--- src/main/java/com/ncuhome/tasklist/service/impl/EmailServiceImpl.java
+++ src/main/java/com/ncuhome/tasklist/service/impl/EmailServiceImpl.java
@@ -1,9 +1,11 @@
 package com.ncuhome.tasklist.service.impl;
 
 import com.ncuhome.tasklist.dataobject.EmailSend;
+import com.ncuhome.tasklist.dataobject.User;
 import com.ncuhome.tasklist.exception.UserRegisterException;
 import com.ncuhome.tasklist.form.UserForm.RegisterForm;
 import com.ncuhome.tasklist.repository.EmailSendRepository;
+import com.ncuhome.tasklist.repository.UserRepository;
 import com.ncuhome.tasklist.service.EmailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,6 +19,9 @@ public class EmailServiceImpl implements EmailService {
     @Autowired
     EmailSendRepository emailSendRepository;
 
+    @Autowired
+    UserRepository userRepository;
+
     @Override
     public Boolean sendEmail(String address, String content) {
         return true;
@@ -24,6 +29,12 @@ public class EmailServiceImpl implements EmailService {
 
     @Override
     public Boolean sendVerifyCode(String address) {
+        // 检查用户
+        User user = userRepository.findByEmail(address);
+        if(user != null){
+            throw new UserRegisterException("用户已存在");
+        }
+
         // 检查邮件格式并发送邮件
         // TODO
         // 将验证码存入数据库
