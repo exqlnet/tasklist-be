ab2a0286fec865f991e0799766995414db0e0b2d
李聪 <591210216@qq.com>
2018-12-07 23:41:22 +0800

    add error handler

--- src/main/java/com/ncuhome/tasklist/handler/LoginHandle.java
+++ src/main/java/com/ncuhome/tasklist/handler/LoginHandle.java
@@ -1,7 +1,9 @@
 package com.ncuhome.tasklist.handler;
 
 import com.ncuhome.tasklist.VO.ResultVO;
+import com.ncuhome.tasklist.exception.TaskException;
 import com.ncuhome.tasklist.exception.UserLoginException;
+import com.ncuhome.tasklist.exception.UserRegisterException;
 import com.ncuhome.tasklist.util.ResultVOUtil;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -9,7 +11,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @ControllerAdvice
 public class LoginHandle {
-    @ExceptionHandler(value = UserLoginException.class)
+    @ExceptionHandler(value = {UserLoginException.class, TaskException.class, UserRegisterException.class})
     @ResponseBody
     public ResultVO handle(Exception e){
         return ResultVOUtil.error(e.getMessage());
