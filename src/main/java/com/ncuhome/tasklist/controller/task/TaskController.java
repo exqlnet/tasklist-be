90eb2f0a11d25b5d1850bae305805354f2bfe9f0
李聪 <591210216@qq.com>
2018-12-08 21:35:48 +0800

    add task con

---
+++ src/main/java/com/ncuhome/tasklist/controller/task/TaskController.java
@@ -0,0 +1,66 @@
+package com.ncuhome.tasklist.controller.task;
+
+
+import com.ncuhome.tasklist.annotations.LoginRequired;
+import com.ncuhome.tasklist.dataobject.Task;
+import com.ncuhome.tasklist.form.TaskForm.CreateTaskForm;
+import com.ncuhome.tasklist.form.TaskForm.DeleteTaskForm;
+import com.ncuhome.tasklist.form.TaskForm.ModifyTaskForm;
+import com.ncuhome.tasklist.service.TaskService;
+import com.ncuhome.tasklist.util.BaseController;
+import com.ncuhome.tasklist.util.ResultVOUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@Slf4j
+@RequestMapping("/api/task")
+public class TaskController extends BaseController {
+
+    @Autowired
+    private TaskService taskService;
+
+    @Autowired
+    ResultVOUtil resultVOUtil;
+
+
+    @LoginRequired
+    @PostMapping("/create")
+    public Object createTask(@RequestBody @Valid CreateTaskForm createTaskForm){
+        taskService.createTask(createTaskForm);
+        return resultVOUtil.success("创建成功");
+    }
+
+    @LoginRequired
+    @DeleteMapping("/delete")
+    public Object deleteTask(@RequestBody @Valid DeleteTaskForm deleteTaskForm){
+        taskService.deleteTask(deleteTaskForm.getTaskId());
+        return resultVOUtil.success("删除成功");
+    }
+
+    @LoginRequired
+    @PutMapping("/modify")
+    public Object modifyTask(@RequestBody @Valid ModifyTaskForm modifyTaskForm){
+        taskService.modifyTask(modifyTaskForm);
+        return resultVOUtil.success("保存成功");
+    }
+
+    @LoginRequired
+    @GetMapping("/list")
+    public Object listTask(){
+        List<Task> list = getUser().getTasks();
+        return resultVOUtil.success(getUser().getTasks());
+    }
+
+    @LoginRequired
+    @PostMapping("/finish")
+    public Object finish(@RequestBody @Valid DeleteTaskForm deleteTaskForm){
+        Integer taskId = deleteTaskForm.getTaskId();
+        taskService.finish(taskId);
+        return resultVOUtil.success("任务已完成");
+    }
+}
---
+++ src/main/java/com/ncuhome/tasklist/form/TaskForm/DeleteTaskForm.java
@@ -0,0 +1,9 @@
+package com.ncuhome.tasklist.form.TaskForm;
+
+
+import lombok.Data;
+
+@Data
+public class DeleteTaskForm {
+    private Integer taskId;
+}
